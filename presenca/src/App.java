27166d602c6938d3d1a12d6148765beae1ba41cc
João Pedro Aiolfi <j.aiolfi@edu.pucrs.br>
2024-03-19 22:09:12 -0300

    oi

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Aula de FDS!");
+        System.out.println("Presente!!");
+        
+
     }
 }
