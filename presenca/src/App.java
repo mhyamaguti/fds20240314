6852b0b0b04ee9d9ecce2a8c56c9c8f95254c42b
Isadora Camargo <143833334+isadoracamarg@users.noreply.github.com>
2024-03-19 21:54:33 -0300

    Presença.java

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Aula de FDS!");
+        System.out.println("Olá =)");
+         System.out.println("Isadora Morari");
     }
 }
