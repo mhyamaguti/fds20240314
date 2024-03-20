14e67bfb168ba898a0ca3d656f40e8e487daaaab
Isadora Fonseca <143005956+isafonsecca@users.noreply.github.com>
2024-03-19 22:04:41 -0300

    Isadora Fonseca

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Aula de FDS!");
-        System.out.println("Isadora Fonseca presente :)");
+        System.out.println("Isadora Fonseca presente ");
     }
 }
